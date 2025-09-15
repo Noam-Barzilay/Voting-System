a154bb46484cdaaf31488c959241ee293c89004a
Noam-Barzilay <barzilay460@gmail.com>
2025-09-15 15:16:14 +0300

    Update app.py
    Separate key for each id
    .env suitable

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ import hashlib
 import time
 import hmac
 import secrets
-from constants import INTERVAL_SIZE, HASH_ID_SERVER_KEY, HASH_TOKEN_SERVER_KEY
+from constants import INTERVAL_SIZE
 import os
-from ciphers import ids_cipher, candidates_cipher
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from dotenv import load_dotenv
+import base64
 
 
 app = Flask(__name__)
@@ -22,6 +24,7 @@ def generate_session_token():
     return token  # 64-char
 
 
+# root directory - start of app
 @app.route('/')
 def index():
     # log in screen
@@ -41,8 +44,17 @@ def generate_otp():
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
 
+    # get hmac key of id
+    hmac_key = os.getenv(f"{user_id}_HMAC_KEY")
+
+    # if id does not exist
+    if hmac_key == None:
+        return "Invalid id"
+
+    hmac_key = base64.b64decode(hmac_key) 
+    
     # search if id exists (based on hash)
-    hashed_id = hmac.new(HASH_ID_SERVER_KEY, user_id.encode(), hashlib.sha256).hexdigest()
+    hashed_id = hmac.new(hmac_key, user_id.encode(), hashlib.sha256).hexdigest()
     
     # update if not present
     cursor.execute("SELECT * FROM Ids WHERE id_hash=(?)", (hashed_id,))
@@ -50,31 +62,37 @@ def generate_otp():
 
     # if row does not exist in table
     if len(Otp_row) == 0:
+        print("here")
         return "Invalid id"
     
     # get nonce
     nonce = Otp_row[0][4]
 
+    # get key and build cipher of user based on the id
+    aes_key = base64.b64decode(os.getenv(f"{user_id}_AES_KEY"))
+    cipher = AESGCM(aes_key)
+
     # decrypt voted
-    voted = ids_cipher.decrypt(nonce, Otp_row[0][1], f"ids_table,hash={hashed_id}".encode())
+    voted = cipher.decrypt(nonce, Otp_row[0][1], f"ids_table,hash={hashed_id}".encode())
 
     # generate otp
-    expire_time = int(time.time()) + INTERVAL_SIZE  # valid for 30s
+    expire_time = int(time.time()) + INTERVAL_SIZE  # valid for INTERVAL_SIZE seconds
     otp = str(secrets.randbelow(10**10)).zfill(10)  # 10-digit random OTP
 
     # create new nonce
     nonce = os.urandom(12)
 
     # encrypt new values and voted too
-    voted_encrypted = ids_cipher.encrypt(nonce, voted, f"ids_table,hash={hashed_id}".encode())
-    otp_encrypted = ids_cipher.encrypt(nonce, otp.encode(), f"ids_table,hash={hashed_id}".encode())
-    encrypted_expire_time = ids_cipher.encrypt(nonce, str(expire_time).encode(), f"ids_table,hash={hashed_id}".encode())
+    voted_encrypted = cipher.encrypt(nonce, voted, f"ids_table,hash={hashed_id}".encode())
+    otp_encrypted = cipher.encrypt(nonce, otp.encode(), f"ids_table,hash={hashed_id}".encode())
+    encrypted_expire_time = cipher.encrypt(nonce, str(expire_time).encode(), f"ids_table,hash={hashed_id}".encode())
 
     # update otp and expire time
     cursor.execute("UPDATE Ids SET voted = (?), otp = (?), expire_time = (?), nonce = (?) WHERE id_hash = (?)",
                     (voted_encrypted, otp_encrypted, encrypted_expire_time, nonce, hashed_id,))
     conn.commit()
     cursor.close()
+    
 
     # Return OTP (realistically send it via email/SMS)
     # jsonify returns a response object to the javascript script the called this api
@@ -91,8 +109,17 @@ def sign_in():
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
 
-    # search if id exists (based on hash)
-    hashed_id = hmac.new(HASH_ID_SERVER_KEY, user_id.encode(), hashlib.sha256).hexdigest()
+    # get hmac key of id
+    hmac_key = os.getenv(f"{user_id}_HMAC_KEY")
+    
+    # if id does not exist
+    if hmac_key == None:
+        return "Invalid id"
+    
+    hmac_key = base64.b64decode(hmac_key)
+    
+    # search for the corresponding row
+    hashed_id = hmac.new(hmac_key, user_id.encode(), hashlib.sha256).hexdigest()
     cursor.execute("SELECT * FROM Ids WHERE id_hash=(?)", (hashed_id,))
     ids_rows = cursor.fetchall()
 
@@ -102,9 +129,13 @@ def sign_in():
     # retieve otp information
     _, voted, otp, expire_time, nonce = ids_rows[0]
 
+    # get key and build cipher of user based on the id
+    aes_key = base64.b64decode(os.getenv(f"{user_id}_AES_KEY"))
+    cipher = AESGCM(aes_key)
+
     # decrypt
-    otp = ids_cipher.decrypt(nonce, otp, f"ids_table,hash={hashed_id}".encode()).decode()
-    expire_time = int(ids_cipher.decrypt(nonce, expire_time, f"ids_table,hash={hashed_id}".encode()).decode())
+    otp = cipher.decrypt(nonce, otp, f"ids_table,hash={hashed_id}".encode()).decode()
+    expire_time = int(cipher.decrypt(nonce, expire_time, f"ids_table,hash={hashed_id}".encode()).decode())
 
     # get cur time stamp
     current_time = int(time.time())
@@ -113,7 +144,7 @@ def sign_in():
     if current_time >= expire_time or user_otp != otp:
         return "Invalid OTP"
 
-    did_vote = ids_cipher.decrypt(nonce, voted, f"ids_table,hash={hashed_id}".encode()).decode()
+    did_vote = cipher.decrypt(nonce, voted, f"ids_table,hash={hashed_id}".encode()).decode()
         
     # if voter have not voted yet, procceed
     if did_vote == '0':
@@ -136,11 +167,14 @@ def get_token(user_id):
         # generate token upon authentication
         token_rows, generated_token, hashed_token = [0], "", ""
 
+        # get hmac key of token
+        hmac_token = base64.b64decode(os.getenv("HMAC_TOKEN_SERVER_KEY"))
+
         # while generated token is already in table, generate a new one
         while (len(token_rows) > 0):
             # generate random token
             generated_token = generate_session_token()
-            hashed_token = hmac.new(HASH_TOKEN_SERVER_KEY, generated_token.encode(), hashlib.sha256).hexdigest()
+            hashed_token = hmac.new(hmac_token, generated_token.encode(), hashlib.sha256).hexdigest()
             cursor.execute("SELECT * FROM Tokens WHERE token_hash=(?)", (hashed_token,))
             token_rows = cursor.fetchall()
 
@@ -154,7 +188,6 @@ def get_token(user_id):
         return render_template('token_screen.html', user_id=user_id, generated_token=generated_token)
 
 
-
 @app.route('/vote/<user_id>', methods=['GET', 'POST'])
 def vote(user_id):
     # connect and load db
@@ -163,17 +196,31 @@ def vote(user_id):
 
     # if we post data
     if request.method == "POST":
+        
+        # get hmac key of id
+        hmac_key = os.getenv(f"{user_id}_HMAC_KEY")
+
+        # if id does not exist
+        if hmac_key == None:
+            return "Invalid id"
+        
+        hmac_key = base64.b64decode(hmac_key)
+
         # check if user already voted - tried going back in the browser
-        hashed_id = hmac.new(HASH_ID_SERVER_KEY, user_id.encode(), hashlib.sha256).hexdigest()
+        hashed_id = hmac.new(hmac_key, user_id.encode(), hashlib.sha256).hexdigest()
         cursor.execute("SELECT * FROM Ids WHERE id_hash=(?)", (hashed_id,))
         ids_rows = cursor.fetchall()
         
         # decrypt
         _, voted, otp, expire_time, ids_nonce = ids_rows[0]
 
-        did_vote = ids_cipher.decrypt(ids_nonce, voted, f"ids_table,hash={hashed_id}".encode()).decode()
-        otp = ids_cipher.decrypt(ids_nonce, otp, f"ids_table,hash={hashed_id}".encode())
-        expire_time = ids_cipher.decrypt(ids_nonce, expire_time, f"ids_table,hash={hashed_id}".encode())
+        # get key and build cipher of user based on the id
+        aes_key = base64.b64decode(os.getenv(f"{user_id}_AES_KEY"))
+        user_cipher = AESGCM(aes_key)
+
+        did_vote = user_cipher.decrypt(ids_nonce, voted, f"ids_table,hash={hashed_id}".encode()).decode()
+        otp = user_cipher.decrypt(ids_nonce, otp, f"ids_table,hash={hashed_id}".encode())
+        expire_time = user_cipher.decrypt(ids_nonce, expire_time, f"ids_table,hash={hashed_id}".encode())
         
         if did_vote == '1':
             return "User already voted!"
@@ -182,8 +229,11 @@ def vote(user_id):
         name = request.form['option']
         token = request.form['token']
 
-        # make sure token is not present yet/not used yet
-        hashed_token = hmac.new(HASH_TOKEN_SERVER_KEY, token.encode(), hashlib.sha256).hexdigest()
+        # get hmac key of token
+        hmac_token = base64.b64decode(os.getenv("HMAC_TOKEN_SERVER_KEY"))
+
+        # make sure token is not used yet
+        hashed_token = hmac.new(hmac_token, token.encode(), hashlib.sha256).hexdigest()
         cursor.execute("SELECT * FROM Tokens WHERE token_hash=(?)", (hashed_token,))
         token_rows = cursor.fetchall()
 
@@ -199,13 +249,17 @@ def vote(user_id):
 
         name, votes, candidate_nonce = candidate_rows[0]
 
+        # get key and build cipher of candidates table
+        aes_key = base64.b64decode(os.getenv("CANDIDATES_CIPHER_KEY"))
+        candidate_cipher = AESGCM(aes_key)
+
         # decrypt candidate's votes count
-        candidate_votes = int(candidates_cipher.decrypt(candidate_nonce, votes, f"candidates_table,name={name}".encode()).decode())
+        candidate_votes = int(candidate_cipher.decrypt(candidate_nonce, votes, f"candidates_table,name={name}".encode()).decode())
 
         # update candidates table and encrypt back (increment candidate_votes)
         # create new nonce
         new_candidate_nonce = os.urandom(12)
-        encrypted_votes = candidates_cipher.encrypt(new_candidate_nonce, str(candidate_votes+1).encode(),
+        encrypted_votes = candidate_cipher.encrypt(new_candidate_nonce, str(candidate_votes+1).encode(),
                                                      f"candidates_table,name={name}".encode())
         cursor.execute("UPDATE Candidates SET votes = (?), nonce = (?) WHERE name=(?)",
                         (encrypted_votes, new_candidate_nonce, name,))
@@ -214,10 +268,11 @@ def vote(user_id):
         # update that voter has voted and encrypt back
         # create new nonce
         new_ids_nonce = os.urandom(12)
+
         # encrypt all values in row
-        voted_encrypted = ids_cipher.encrypt(new_ids_nonce, b'1', f"ids_table,hash={hashed_id}".encode())
-        otp_encrypted = ids_cipher.encrypt(new_ids_nonce, otp, f"ids_table,hash={hashed_id}".encode())
-        expire_time_encrypted = ids_cipher.encrypt(new_ids_nonce, expire_time, f"ids_table,hash={hashed_id}".encode())
+        voted_encrypted = user_cipher.encrypt(new_ids_nonce, b'1', f"ids_table,hash={hashed_id}".encode())
+        otp_encrypted = user_cipher.encrypt(new_ids_nonce, otp, f"ids_table,hash={hashed_id}".encode())
+        expire_time_encrypted = user_cipher.encrypt(new_ids_nonce, expire_time, f"ids_table,hash={hashed_id}".encode())
 
         cursor.execute("UPDATE Ids SET voted = (?), OTP = (?), expire_time = (?), nonce = (?) WHERE id_hash=(?)",
                         (voted_encrypted, otp_encrypted, expire_time_encrypted, new_ids_nonce, hashed_id,))
@@ -248,6 +303,7 @@ def open_browser():
 
 
 if __name__ == '__main__':
+    load_dotenv()
     # open once upon call for main
     threading.Timer(1, open_browser).start()
     app.run(debug=False)
