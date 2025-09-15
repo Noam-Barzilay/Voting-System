4a21e025af8cfa0fc3bfd36d67eac0851c03d852
Noam-Barzilay <barzilay460@gmail.com>
2025-09-15 15:12:00 +0300

    Delete ciphers.py

--- ciphers.py
+++
@@ -1,4 +0,0 @@
-from constants import CANDIDATES_CIPHER_KEY, IDS_CIPHER_KEY
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-candidates_cipher = AESGCM(CANDIDATES_CIPHER_KEY)
-ids_cipher = AESGCM(IDS_CIPHER_KEY)
