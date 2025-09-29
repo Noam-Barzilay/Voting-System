d9309ab09db7b7cc792c2d689800cd6cbc439399
Noam-Barzilay <barzilay460@gmail.com>
2025-09-29 09:54:24 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 candidates = ["Bob", "John", "Alice", "Jack", "Eric", "Hillary"]
 # in real life - extract real ids of eligible people to vote
-NUM_OF_VOTERS = 10
+NUM_OF_VOTERS = 100
 ids = [str(n) for n in range(1, NUM_OF_VOTERS + 1)]
 INTERVAL_SIZE = 60  # 60 seconds
+
